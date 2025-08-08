801b6ebe93a1884624c969f7cd2c780fb2c8bc09
ringii21 <anais.bonard@gmail.com>
2025-08-08 23:08:44 +0200

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const session = require('express-session');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const users = JSON.parse(fs.readFileSync('./users.json'));
 
